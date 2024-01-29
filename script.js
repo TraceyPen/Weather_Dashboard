9f0162ffeb1367345bc23022cbf7f0eb5deb2b11
TraceyPen <traceylpender@yahoo.co.uk>
2024-01-29 21:07:23 +0000

    working on isolating todays weather information

--- script.js
+++ script.js
@@ -2,50 +2,82 @@
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
 
+var latNumber
+var lonNumber
+var countryName
+var stateName
+var mainWeather
+var temp
+var wind
+var humidity
 
 
 searchButton.addEventListener('click', function() {
+  lonNumber = ""
+  latNumber = ""
   // get the search input value
   const searchTerm = searchInput.value;
   // do something with the search term (e.g. redirect to a search results page)
   console.log(`Searching for "${searchTerm}"...`);
 
-  var coordsURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchTerm + "&limit=1&appid=90a95552c0fa8f37656e8d048d51f371"
-  fetch(coordsURL)
+  var coordsURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchTerm + "&limit=1&appid=90a95552c0fa8f37656e8d048d51f371";
+
+      fetch(coordsURL)
       .then(function (response) {
-        return response.json();
+         return response.json();
       })
-      .then(function (data) {
+      .then (function (data) {
         console.log(JSON.stringify(data))
-       // $("#forecast").text(JSON.stringify(data));
-       var latNumber = data[0].lat;
-       console.log(latNumber);
-       var lonNumber = data[0].lon;
-       console.log(lonNumber);
-       var countryName = data[0].country;
-       console.log(countryName);
-       var stateName = data[0].state;
-       console.log(stateName);
        
+       latNumber = data[0].lat;
+       console.log(latNumber);
+       lonNumber = data[0].lon;
+       console.log(lonNumber);
+       countryName = data[0].country;
+       console.log(countryName);
+       stateName = data[0].state;
+       console.log(stateName);
+
+
+       //if statement pausing the code until latNumber and lonNumber are not empty strings
+       if (lonNumber === !"" && latNumber === !"");
+       var todayURL = "https://api.openweathermap.org/data/2.5/weather?lat="+latNumber+"&lon="+lonNumber+"&appid=90a95552c0fa8f37656e8d048d51f371"
+       // var todayURL = "https://api.openweathermap.org/data/2.5/weather?lat=52.4796992&lon=-1.9026911&appid=90a95552c0fa8f37656e8d048d51f371"
+         fetch(todayURL)
+            .then(function (response) {
+              return response.json();
+            })
+            .then(function (data) {
+              console.log(JSON.stringify(data))
+             
+              mainWeather = data[1].main;
+              console.log(mainWeather);// WORKING ON THIS BIT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+              // temp = data[0].lon;
+              // console.log(lonNumber);
+              // wind = data[0].country;
+              // console.log(countryName);
+              // humidity = data[0].state;
+              // console.log(stateName);
+            });
+
+
+
+
+
       });
 
       //------------------------------------------------------------------------------------------------------------
+
+//if (lonNumber === !"" && latNumber === !"");
+      
 // api call for today's weather https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
 
-      var todayURL = https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=90a95552c0fa8f37656e8d048d51f371"
-      fetch(coordsURL)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (data) {
-            console.log(JSON.stringify(data))
-           // $("#forecast").text(JSON.stringify(data));
-           var latNumber = data[0].lat;
-           console.log(latNumber);
-           var lonNumber = data[0].lon;
-           console.log(lonNumber);
-          });
+   
+
+
+
+});
 
 
 
@@ -65,7 +97,7 @@ searchButton.addEventListener('click', function() {
 
 
 //end of search button event listener
-});
+
 
 
 
